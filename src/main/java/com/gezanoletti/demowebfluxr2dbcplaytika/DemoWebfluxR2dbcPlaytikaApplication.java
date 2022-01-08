e385ee5082e695cdc1fc0a6e5c198c58191fc5c9
gezanoletti <gezanoletti@gmail.com>
2022-01-08 17:10:17 +0100

    first commit

---
+++ src/main/java/com/gezanoletti/demowebfluxr2dbcplaytika/DemoWebfluxR2dbcPlaytikaApplication.java
@@ -0,0 +1,13 @@
+package com.gezanoletti.demowebfluxr2dbcplaytika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebfluxR2dbcPlaytikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWebfluxR2dbcPlaytikaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gezanoletti/demowebfluxr2dbcplaytika/DemoWebfluxR2dbcPlaytikaApplicationTests.java
@@ -0,0 +1,15 @@
+package com.gezanoletti.demowebfluxr2dbcplaytika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebfluxR2dbcPlaytikaApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
